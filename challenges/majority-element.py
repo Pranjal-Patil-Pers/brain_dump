a6b039c58e076d8d79c6f7fdf93e7be4032c14a8
Pranjal Vijaysing Patil <patilpranjal248@gmail.com>
2026-03-05 15:34:38 -0500

    Refactor majorityElement to use dictionary for counting
    Refactor majorityElement method to use a dictionary for counting occurrences instead of a lead variable.

--- challenges/majority-element.py
+++ challenges/majority-element.py
@@ -1,16 +1,19 @@
 class Solution():
-    def majorityElement(self, nums):
-        lead = 0
-        count =0 
+    def majorityElement(self,nums):
+        dict1 = {}
+        maxOcc=0
+        me = float("-inf")
         for num in nums:
-            if count ==0:
-                lead = num
-            if num != lead:
-                count-=1
-            elif num == lead:
-                count+=1
-        return lead
-
+            print(dict1)
+            if num in dict1:
+                dict1[num] = dict1[num]+1
+            else:
+                dict1[num]=1
+            if maxOcc < dict1[num]:
+                maxOcc = dict1[num]
+                me = num
+        return me
+            
 if __name__== '__main__':
     sol = Solution()
     print("output:",sol.majorityElement([2,3,4,2]))
